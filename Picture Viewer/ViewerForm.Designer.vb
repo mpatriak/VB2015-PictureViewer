c8e1b887ac3b768a0aa9cf2ff5cfb6e7b5b0aa87
Michal Patriak <mpatriak15@outlook.com>
2016-06-11 17:05:04 -0700

    Picture Viewer v1.0

--- Picture Viewer/ViewerForm.Designer.vb
+++ Picture Viewer/ViewerForm.Designer.vb
@@ -22,17 +22,64 @@ Partial Class ViewerForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ViewerForm))
+        Me.btnSelectPicture = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.picShowPicture = New System.Windows.Forms.PictureBox()
+        Me.ofdSelectPicture = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.picShowPicture, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'btnSelectPicture
+        '
+        Me.btnSelectPicture.Location = New System.Drawing.Point(295, 10)
+        Me.btnSelectPicture.Name = "btnSelectPicture"
+        Me.btnSelectPicture.Size = New System.Drawing.Size(85, 23)
+        Me.btnSelectPicture.TabIndex = 0
+        Me.btnSelectPicture.Text = "Select Picture"
+        Me.btnSelectPicture.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(295, 40)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(85, 23)
+        Me.btnQuit.TabIndex = 1
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'picShowPicture
+        '
+        Me.picShowPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.picShowPicture.Location = New System.Drawing.Point(8, 8)
+        Me.picShowPicture.Name = "picShowPicture"
+        Me.picShowPicture.Size = New System.Drawing.Size(282, 275)
+        Me.picShowPicture.TabIndex = 2
+        Me.picShowPicture.TabStop = False
+        '
+        'ofdSelectPicture
+        '
+        Me.ofdSelectPicture.Filter = "PNG Files|*.png|Windows Bitmaps|*.bmp|JPEG Files|*.jpg"
+        Me.ofdSelectPicture.Title = "Select Picture"
         '
         'ViewerForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(384, 286)
+        Me.Controls.Add(Me.picShowPicture)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnSelectPicture)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "ViewerForm"
         Me.Text = "Picture Viewer"
+        CType(Me.picShowPicture, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents btnSelectPicture As Button
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents picShowPicture As PictureBox
+    Friend WithEvents ofdSelectPicture As OpenFileDialog
 End Class
--- Picture Viewer/ViewerForm.vb
+++ Picture Viewer/ViewerForm.vb
@@ -1,3 +1,16 @@
 ﻿Public Class ViewerForm
+    Private Sub btnSelectPicture_Click(sender As Object, e As EventArgs) Handles btnSelectPicture.Click
+        ' Show the open file dialog box
+        If ofdSelectPicture.ShowDialog = DialogResult.OK Then
+            ' Load the picture into the picture box.
+            picShowPicture.Image = Image.FromFile(ofdSelectPicture.FileName)
+            ' Show the name of the file in the form's caption.
+            Me.Text = "Picture Viewer (" & ofdSelectPicture.FileName & ")"
+        End If
+    End Sub
 
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        ' Close the window and exit the application.
+        Me.Close()
+    End Sub
 End Class
